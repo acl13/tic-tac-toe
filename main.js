2ce5a0c653faa0af09345144a909c61200406680
acl13 <128934431+acl13@users.noreply.github.com>
2024-04-09 16:13:47 -0400

    create Gameboard

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('test');
+
+function Gameboard() {
+    board = [];
+    const rows = 3;
+    const columns = 3;
+
+    for (i = 0; i < rows; i++) {
+        board[i] = [];
+        for (j = 0; j < columns; j++) {
+            const cell = "";
+            board[i].push(cell);
+        }
+    }
+    console.log(board);
+    getBoard = () => board;
+    return getBoard;
+}
+
+Gameboard();
